dbafca14630c834b4744e27b26d52f38475126f2
Emre Yılmaz <emreyyilmaz34@gmail.com>
2024-10-07 14:01:14 +0300

    Initial Commit

---
+++ todoapp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  todoapp
+//
+//  Created by Emre Yılmaz on 7.10.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ todoapp/todoappApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoappApp.swift
+//  todoapp
+//
+//  Created by Emre Yılmaz on 7.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct todoappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
